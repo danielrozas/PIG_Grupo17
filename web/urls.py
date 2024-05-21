239eac3b0ab12918df65ea1b75ca59f61e2ca8a8
danielrozas <drfceia@gmail.com>
2024-05-20 23:14:17 -0300

    Creacion del Directorio templates

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     # Cuando entre a localhost/ nada la vista que buscará será 
     # la que tiene el nombre index
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('saludar/<str:nombre>', views.saludar, name='saludar'),
+    re_path('^alumnos_por_anio/(?P<year>[0-9]{4})/$', views.alumnos_por_año, name='alumnos_por_anio')
 ]
--- web/views.py
+++ web/views.py
@@ -4,4 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hola Mundo Django")
+    return render(request, 'index.html')
+
+def saludar(request, nombre):
+    return HttpResponse(f"<h1>Bienvenido {nombre}</h1>")
+
+def alumnos_por_año(request, year):
+    alumnos = ["Carlos", "Maria", "Jose"]
+    return HttpResponse(f"Listado de Alumnos: {year} \n {alumnos}")
