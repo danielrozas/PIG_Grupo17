19ab0eb90f50e4c3eb7448ce894e2c50f311ea69
danielrozas <drfceia@gmail.com>
2024-05-23 18:17:00 -0300

    Templates 1

--- web/views.py
+++ web/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import datetime
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+
+    # Agrego el diccionario context con dos variables de contexto
+    context ={
+        'nombre': 'Carlos',
+        'fecha_hora': datetime.datetime.now()
+    }
+
+    return render(request, 'web/index.html', context)
 
 def saludar(request, nombre):
     return HttpResponse(f"<h1>Bienvenido {nombre}</h1>")
@@ -12,3 +21,7 @@ def saludar(request, nombre):
 def alumnos_por_año(request, year):
     alumnos = ["Carlos", "Maria", "Jose"]
     return HttpResponse(f"Listado de Alumnos: {year} \n {alumnos}")
+
+def listado_alumnos(request):
+    contexto={}
+    return render(request, 'web/listado_alumnos.html', contexto)
