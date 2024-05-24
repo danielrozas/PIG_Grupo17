a4fa8c3fd1baab54bd788221db4a2c9b1f547dad
danielrozas <drfceia@gmail.com>
2024-05-24 10:17:54 -0300

    Configuracion de DATABASE_SETTINGS

--- PIG_Grupo17/settings.py
+++ PIG_Grupo17/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'PIG_Grupo17.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "pig_grupo17",
+        "USER": "postgres",
+        "PASSWORD": "accos50",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
