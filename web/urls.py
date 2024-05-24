63ac98adad64d8ba1e04ad18a797cc5f629ada85
danielrozas <drfceia@gmail.com>
2024-05-24 11:48:30 -0300

    .

---
+++ web/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    # Cuando entre a localhost/ nada la vista que buscará será 
+    # la que tiene el nombre index
+    path('', views.index, name='index'),
+    path('saludar/<str:nombre>', views.saludar, name='saludar'),
+    re_path('^alumnos_por_anio/(?P<year>[0-9]{4})/$', views.alumnos_por_año, name='alumnos_por_anio'),
+    path('listado_alumnos', views.listado_alumnos, name='lista_alumnos')
+]
