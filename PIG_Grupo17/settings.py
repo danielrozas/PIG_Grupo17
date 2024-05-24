1b71cfcdadbe42cf8a56db0c308c87ce5af89337
danielrozas <drfceia@gmail.com>
2024-05-24 15:04:55 -0300

    Definicion de modelos

--- PIG_Grupo17/settings.py
+++ PIG_Grupo17/settings.py
@@ -10,14 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import environ
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-# reading .env file
-environ.Env.read_env()
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-)e@vo!ev!vdt&k+s6u2gfh_9qpoev+!ziy@-hpz30n_2j65$_('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -83,8 +75,16 @@ WSGI_APPLICATION = 'PIG_Grupo17.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db(), 
+    'default': {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "pig_grupo17",
+        "USER": "postgres",
+        "PASSWORD": "accos50",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- web/models.py
+++ web/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Administradores(models.Model):
+    idAdministrador = models.AutoField(primary_key=True) # Esto equivale a INTEGER UNSIGNED NOT NULL
+    NombreAdministrador = models.CharField(max_length=60, null = False)
+    ContrasenaAdministrador =models.CharField(max_length=20, null = False)
+
+class Actores(models.Model):
+    idActor = models.AutoField(primary_key=True) # Esto equivale a INTEGER UNSIGNED NOT NULL
+    NombreActor = models.CharField(max_length=60, null = False)
+
+class Directores(models.Model):
+    idDirector = models.AutoField(primary_key=True) # Esto equivale a INTEGER UNSIGNED NOT NULL
+    NombreDirector = models.CharField(max_length=60, null = False)
+
+class Peliculas(models.Model):
+    idPelicula = models.AutoField(primary_key=True) # Esto equivale a INTEGER UNSIGNED NOT NULL
+    NombrePelicula = models.CharField(max_length=128, null = False) 
+    Director_id = models.ForeignKey(Directores, on_delete=models.CASCADE)
+    FechaEstreno = models.DateTimeField(blank=True, null=True)
+    URLCartel = models.CharField(max_length=150, null = True)
+
+class ActoresenPeliculas(models.Model):
+     idActoresenPeliculas = models.AutoField(primary_key=True) # Esto equivale a INTEGER UNSIGNED NOT NULL
+     Actor_id = models.ForeignKey(Actores, on_delete=models.CASCADE)    
+     Pelicula_id = models.ForeignKey(Peliculas, on_delete=models.CASCADE)
+     Papel = models.CharField(max_length=128, null = False)
