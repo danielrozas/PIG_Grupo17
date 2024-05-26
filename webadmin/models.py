e7e7a52bc6c665c7a3261bc1faf825aaa9095878
danielrozas <drfceia@gmail.com>
2024-05-26 17:28:44 -0300

    Def. de models en webadmin

--- webadmin/models.py
+++ webadmin/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
 # Create your models here.
+
+class Administradores(models.Model):
+    idAdministrador = models.AutoField(primary_key=True) # Esto equivale a INTEGER UNSIGNED NOT NULL
+    NombreAdministrador = models.CharField(max_length=60, null = False)
+    ContrasenaAdministrador =models.CharField(max_length=20, null = False)
+
+    def __str__(self):
+        return self.NombreAdministrador
+        
+class Actores(models.Model):
+    idActor = models.AutoField(primary_key=True) # Esto equivale a INTEGER UNSIGNED NOT NULL
+    NombreActor = models.CharField(max_length=60, null = False)
+
+    def __str__(self):
+        return self.NombreActor
+
+class Directores(models.Model):
+    idDirector = models.AutoField(primary_key=True) # Esto equivale a INTEGER UNSIGNED NOT NULL
+    NombreDirector = models.CharField(max_length=60, null = False)
+
+    def __str__(self):
+        return self.NombreDirector
+
+class Generos(models.Model):
+    idGenero = models.AutoField(primary_key=True) # Esto equivale a INTEGER UNSIGNED NOT NULL
+    NombreGenero = models.CharField(max_length=60, null = False)
+
+    def __str__(self):
+        return self.NombreGenero
+
+class Peliculas(models.Model):
+    idPelicula = models.AutoField(primary_key=True) # Esto equivale a INTEGER UNSIGNED NOT NULL
+    NombrePelicula = models.CharField(max_length=128, null = False) 
+    Director_id = models.ForeignKey(Directores, on_delete=models.CASCADE)
+    Genero_id = models.ForeignKey(Generos, on_delete=models.CASCADE)
+    FechaEstreno = models.DateTimeField(blank=True, null=True)
+    URLCartel = models.CharField(max_length=150, null = True)
+
+    def __str__(self):
+        return self.NombrePelicula
+
+class ActoresenPeliculas(models.Model):
+    idActoresenPeliculas = models.AutoField(primary_key=True) # Esto equivale a INTEGER UNSIGNED NOT NULL
+    Actor_id = models.ForeignKey(Actores, on_delete=models.CASCADE)    
+    Pelicula_id = models.ForeignKey(Peliculas, on_delete=models.CASCADE)
+    Papel = models.CharField(max_length=128, null = False)
+
+    def __str__(self):
+        return f"{self.Actor_id} {self.Pelicula_id}"
+
--- webadmin/views.py
+++ webadmin/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import (CreateView,
+                                  DetailView,
+                                  UpdateView,
+                                  DeleteView,
+                                  ListView, 
+                                  TemplateView
+                                  )
+
+
+import datetime
+
+
 
 # Create your views here.
+
+from .models import *
+
+# ====================================
+# Actores
+# ====================================
+
+class ActoresCreateView(CreateView):
+    model = Actores
+    template_name = "actor/add.html"
+
+    fields = [
+        'NombreActor'
+    ]
+ 
+    success_url = reverse_lazy('web_app:correcto')
+
+    def form_valid(self, form):
+        # Logica del Proceso
+        actor = form.save(commit=False)
+        actor.save()
+        return super(ActoresCreateView, self).form_valid(form)
+
+
+class SuccessView(TemplateView):
+        template_name = "actor/success.html"
+
+# ====================================        
