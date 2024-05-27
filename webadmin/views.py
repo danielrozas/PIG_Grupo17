8703577dbf10aa3be353e0c14098b9b818e089fd
danielrozas <drfceia@gmail.com>
2024-05-27 00:22:02 -0300

    Menues Administracion

--- webadmin/views.py
+++ webadmin/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import (CreateView,
                                   DetailView,
@@ -12,19 +11,25 @@ from django.views.generic import (CreateView,
 
 import datetime
 
-
-
 # Create your views here.
 
 from .models import *
 
+# def index(request):
+#     return render(request, 'webadmin/index.html')
+
+class InicioView(TemplateView):
+    """Vista que carga la página de inicio"""
+    template_name= 'webadmin/index.html'
+
 # ====================================
 # Actores
 # ====================================
-
-class ActoresCreateView(CreateView):
+# Agregar Actor
+# ====================================
+class ActorCreateView(CreateView):
     model = Actores
-    template_name = "actor/add.html"
+    template_name = "webadmin/Actores/add_actor.html"
 
     fields = [
         'NombreActor'
@@ -36,10 +41,75 @@ class ActoresCreateView(CreateView):
         # Logica del Proceso
         actor = form.save(commit=False)
         actor.save()
-        return super(ActoresCreateView, self).form_valid(form)
+        return super(ActorCreateView, self).form_valid(form)
+
+# ====================================
+# Mostrar Actor
+# ====================================
+class ActorDetailView(DetailView):
+    model = Actores
+    template_name = "webadmin/Actores/read_actor.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ActorDetailView, self).get_context_data(**kwargs)
+        #
+        # Aca pongo un proceso para recuperar el actor
+        #
+        context['titulo'] = 'Empleado del Mes'
+        return context
+    
+
+# ====================================
+# Actualizar Actor
+# ====================================
+class ActorUpdateView(UpdateView):
+        model = Actores
+        template_name = "webadmin/Actores/update_actor.html"
+        fields = [
+        'NombreActor'
+        ]
+
+        success_url = reverse_lazy('persona_app:correcto')
+
+        def post(self, request, *args, **kwargs):
+            self.object = self.get_object()
+            print('****** METODO post ******')
+            print('=========================')
+            print(request.POST)
+            print(request.POST['NombreActor'])
+            return super().post(request, *args, **kwargs)
+
+        def form_valid(self, form):
+            # Logica del Proceso
+            print('****** METODO form_valid ******')         
+            return super(ActorUpdateView, self).form_valid(form)  
+
+   
+
+# ====================================
+# Borrar Actor
+# ====================================
+class ActorDeleteView(DeleteView):
+        model = Actores
+        template_name = "webadmin/Actores/delete_actor.html"
+        success_url = reverse_lazy('persona_app:correcto')
+
+
 
 
 class SuccessView(TemplateView):
         template_name = "actor/success.html"
 
-# ====================================        
+
+# ====================================    
+# 
+class OpcionesTablaView(TemplateView):
+    template_name = 'webadmin/opciones_tabla.html'
+
+    # Para pasar variables 
+    def get_context_data(self, **kwargs):
+        context = super(OpcionesTablaView, self).get_context_data(**kwargs)
+        # If don´t call 'super', you wont have the context processor variables
+        # like 'user'
+        context['Nombre_de_Archivo'] = ""  # 'var content' to add template variables
+        return context
