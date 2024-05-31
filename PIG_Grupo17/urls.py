d0e925b21b66ca8109fc85c930dbd30f3a8429f0
danielrozas <drfceia@gmail.com>
2024-05-30 22:52:03 -0300

    App webadmin borrada

--- PIG_Grupo17/urls.py
+++ PIG_Grupo17/urls.py
@@ -17,10 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from web import views
-from webadmin import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('web/', include('web.urls')),
-    path('webadmin/', include('webadmin.urls'))
+    path('web/', include('web.urls'))
 ]
--- web/urls.py
+++ web/urls.py
@@ -1,20 +1,8 @@
 from django.urls import path, re_path
 from . import views
 
-app_name="web_app"
 
 urlpatterns = [
-    # Cuando entre a localhost/ nada la vista que buscará será 
-    # la que tiene el nombre index
-    # path('', views.index, name='index'),
-
-    path('add-actor', views.ActoresCreateView.as_view(), name='agregar-actor'),
-
-    path('success/', 
-        views.SuccessView.as_view(), 
-        name='correcto'
-    ),
-
     # path('saludar/<str:nombre>', views.saludar, name='saludar'),
     # re_path('^alumnos_por_anio/(?P<year>[0-9]{4})/$', views.alumnos_por_año, name='alumnos_por_anio'),
     # path('listado_alumnos', views.listado_alumnos, name='lista_alumnos')
