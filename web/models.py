94e06eaab0fb1248cf2e487e9c672a9026748ce2
danielrozas <drfceia@gmail.com>
2024-05-31 23:18:38 -0300

    Lee Cliente y lo muestra cliente_detail

--- web/models.py
+++ web/models.py
@@ -8,11 +8,23 @@ class Clientes(models.Model):
     Email =models.EmailField(max_length=45, verbose_name="Email")
     Direccion =  models.CharField(max_length=45, verbose_name="Direccion")
 
+    # Este metodo provee una representaacion leible por una persona
+    # de la instancia del modelo. Es particularmente util cuando se trabaja 
+    # con la interface admin de Django
+    def __str__(self):
+        return self.NombreCliente
+
 class Directores(models.Model):
     NombreDirector = models.CharField(max_length=60, verbose_name="NombreDirector")
 
+    def __str__(self):
+        return self.NombreDirector
+
 class Generos(models.Model):
     DescripcionGenero = models.CharField(max_length=60, verbose_name="DescripcionGenero")
+
+    def __str__(self):
+        return self.DescripcionGenero
 
 class Peliculas(models.Model):
     TituloPelicula = models.CharField(max_length=128, verbose_name="TituloPelicula") 
--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, re_path
+
 from . import views
 
 
 urlpatterns = [
-    # path('saludar/<str:nombre>', views.saludar, name='saludar'),
-    # re_path('^alumnos_por_anio/(?P<year>[0-9]{4})/$', views.alumnos_por_año, name='alumnos_por_anio'),
-    # path('listado_alumnos', views.listado_alumnos, name='lista_alumnos')
+    path('', views.index, name='index'),
+    path('cliente', views.ClienteDetailView.as_view(), name='cliente_detail'),
+
+
 ]
