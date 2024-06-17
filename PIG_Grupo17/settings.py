0d088f42cd6b5fa4e698d2da1719de88e3189a0f
fedePeralta9 <71795690+fedePeralta9@users.noreply.github.com>
2024-06-17 14:50:23 -0300

    cambio de contraseña

--- PIG_Grupo17/settings.py
+++ PIG_Grupo17/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     "ENGINE": "django.db.backends.postgresql",
     "NAME": "pig_grupo17",
     "USER": "postgres",
-    "PASSWORD": "accos50",
+    "PASSWORD": "4257",
     "HOST": "127.0.0.1",
     "PORT": "5432",
  }
